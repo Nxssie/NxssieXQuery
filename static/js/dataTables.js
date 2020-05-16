a56a0fa774a2f1ec2f0324a4bc345b2e06b1ce09
Nxssie <nxssiedev@gmail.com>
2020-05-16 01:52:51 +0100

    adding return home button

--- static/js/dataTables.js
+++ static/js/dataTables.js
@@ -1,7 +1,9 @@
 export let dataTable = () => {
     $(document).ready(function () {
         $('#songs-table').DataTable({
-            "bLengthChange": false
+            "bLengthChange": false,
+            bFilter: false,
+            bInfo: false
         });
         $('#songs-table_wrapper').find('label').each(function () {
             $(this).parent().append($(this).children());
--- static/js/loader.js
+++ static/js/loader.js
@@ -8,7 +8,7 @@ let loadJumbo = () => {
         <div class="text-white text-center rgba-stylish-strong py-5 px-4">
             <div class="py-5">
                 <!-- Content -->
-                <h5 class="h5"> <img id="logo" src="static/img/logo.png"/> </i> </h5>
+                <h5 class="h5"><img id="logo" src="../static/img/logo.png"/></h5>
                 <a class="btn purple-gradient" href="https://tidal.com/browse/artist/9314580">Tidal</a>
                 <a class="btn peach-gradient" href="https://github.com/Nxssie/NxssieXQuery"><i class="fas fa-clone left"></i> View project</a>
                 <a class="btn aqua-gradient" href="https://open.spotify.com/artist/6p9nrPaCQXasjQbaMZ2gv8"><i class="fab fa-spotify left"></i> Spotify</a>
@@ -18,8 +18,16 @@ let loadJumbo = () => {
     `
 }
 
+let goBack = () => {
+    window.history.back();
+    console.log("hello");
+}
+
 function initialize() {
     document.getElementById("jumbo").innerHTML = loadJumbo();
+    if(document.getElementById("return-button")){
+        document.getElementById("return-button").addEventListener("click", goBack());
+    }
     dataTable();
 }
 
